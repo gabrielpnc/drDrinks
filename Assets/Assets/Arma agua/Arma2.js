8ba20c7371815fa8cc3a0a998fe7d8163c7e1201
Gabriel Duran <gabriel_pnc@hotmail.com>
2016-11-24 11:39:55 -0200

    Coloquei a segunda arma, atirando vermelho.

---
+++ Assets/Assets/Arma agua/Arma2.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+var projetil: GameObject;
+function Start () {
+
+}
+
+function Update () {
+if(Input.GetButtonDown("Fire2"))
+{
+Instantiate(projetil,transform.position,transform.rotation);
+}
+}
